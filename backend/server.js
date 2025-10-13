028343471cb67429df68bccf7afc60579a5877d1
Nastia Bzova <bzovanastia@gmail.com>
2025-10-14 01:04:43 +0200

    Postgre -Connection Config (using environment vars)

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,12 @@ const port = process.env.PORT;
 app.use(cors());
 //parsing
 app.use(express.json());
+
+// Postgre Conn Config: 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
