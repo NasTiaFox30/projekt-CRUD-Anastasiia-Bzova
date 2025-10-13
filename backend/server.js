749f59641ad2e63eae9362fc6e645b08324f400e
Nastia Bzova <bzovanastia@gmail.com>
2025-10-14 01:31:03 +0200

    Fix: name of Table

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const pool = new Pool({
 app.get('/tasks', async (req, res) => {
   try {
     console.log("> GET all Tasks");
-    const result = await pool.query('SELECT * FROM tasks ORDER BY created_at DESC');
+    const result = await pool.query('SELECT * FROM Tasks ORDER BY created_at DESC');
     res.json(result.rows);
   } catch (error) {
     console.error('Error fetch data', error);
@@ -43,7 +43,7 @@ app.get('/tasks/:id', async (req, res) => {
   try {
     const { id } = req.params;
     console.log(`> GET task by ID: ${id}`);
-    const result = await pool.query('SELECT * FROM tasks WHERE id = $1', [id]);
+    const result = await pool.query('SELECT * FROM Tasks WHERE id = $1', [id]);
     
     if (result.rows.length === 0) {
       return res.status(404).json({ error: 'Task not found!' });
@@ -68,7 +68,7 @@ app.post('/tasks', async (req, res) => {
     }
     
     const result = await pool.query(
-      'INSERT INTO tasks (title_name, description, deadline_date, priority, status) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      'INSERT INTO Tasks (title_name, description, deadline_date, priority, status) VALUES ($1, $2, $3, $4, $5) RETURNING *',
       [title_name.trim(), description?.trim(), deadline_date, priority, status]
     );
     
