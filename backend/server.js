9814cc6cc66376d1456308b6ea94431c6a75411d
Nastia Bzova <bzovanastia@gmail.com>
2025-10-14 01:13:58 +0200

    update(GET /tasks) - added console logs

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,11 @@ const pool = new Pool({
 // GET /tasks 
 app.get('/tasks', async (req, res) => {
   try {
+    console.log("> GET all Tasks");
     const result = await pool.query('SELECT * FROM tasks ORDER BY created_at DESC');
     res.json(result.rows);
   } catch (error) {
+    console.error('Error fetch data', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
