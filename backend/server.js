c2a799d6a5fe3f682b85ea2d234a5db68c7b982b
Nastia Bzova <bzovanastia@gmail.com>
2025-10-14 01:16:47 +0200

    Create Route:  GET /tasks/:id
    find task by its ID
    error respond 404 - not find task
    error respond status - 500 (if there is an error fetching)

--- backend/server.js
+++ backend/server.js
@@ -37,3 +37,21 @@ app.get('/tasks', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+// GET /tasks/:id 
+app.get('/tasks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(`> GET task by ID: ${id}`);
+    const result = await pool.query('SELECT * FROM tasks WHERE id = $1', [id]);
+    
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Task not found!' });
+    }
+    
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Error fetch data', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
