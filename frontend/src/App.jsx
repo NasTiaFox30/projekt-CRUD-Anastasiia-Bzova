1eef65c4055864522a572e12577f789d1d4b4828
Anastasiia Bzova <bzovanastia@gmail.com>
2025-10-16 21:37:24 +0200

    update API_URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,9 @@ import './App_anim.css';
 import './App_adapt.css';
 import Footer from './Footer';
 
-const API_URL = 'http://localhost:3001/tasks';
+const API_URL = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/tasks`
+  : 'http://localhost:3001/tasks';
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
