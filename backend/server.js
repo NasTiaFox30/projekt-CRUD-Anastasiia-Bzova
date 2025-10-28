a8dd34b19514233808a3f1d432d9cbad8d953304
Anastasiia Bzova <bzovanastia@gmail.com>
2025-10-28 09:53:45 +0100

    Backend(server.js): Added imports bcrypt & jwt

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import pkg from 'pg';
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
