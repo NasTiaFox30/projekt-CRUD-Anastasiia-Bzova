9e4c517b67ced50a0d3e9867fe5ac6f07ecc36a0
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-07 20:03:34 +0100

    Created Script migrateDB.js + imports

---
+++ database/scripts/migrateDB.js
@@ -0,0 +1,6 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import readline from 'readline';
