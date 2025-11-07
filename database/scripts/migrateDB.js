6b5e162dec721dd721f7beb81b1575a4385eadd1
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-07 20:06:53 +0100

    Fucntion AskQuestion() added

--- database/scripts/migrateDB.js
+++ database/scripts/migrateDB.js
@@ -18,3 +18,9 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+function askQuestion(question) {
+  return new Promise((resolve) => {
+    rl.question(question, resolve);
+  });
+}
